23615a93e6131f959e60d82b406fcad9aa862d2f
Rutabadev <etienner37@gmail.com>
2018-07-24 14:57:07 +0200

    test firebase login

--- src/login/index.js
+++ src/login/index.js
@@ -1,3 +1,45 @@
-export const Login = () => {
-  return <button onClick={() => alert("Pas encore fait.")}>Login</button>;
-};
+import { Component } from "preact";
+import { auth, provider } from "../firebase";
+import "./style.scss";
+
+export class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+  }
+
+  login() {
+    auth.signInWithPopup(provider).then(result => {
+      const user = result.user;
+      this.setState({
+        user
+      });
+    });
+  }
+
+  logout() {
+    auth.signOut().then(() => {
+      this.setState({
+        user: null
+      });
+    });
+  }
+
+  user() {
+    if (this.state.user) {
+      return (
+        <button onClick={this.logout}>
+          <img src={this.state.user.photoURL} alt="User profile" />
+        </button>
+      );
+    } else {
+      return <button onClick={this.login}>Login</button>;
+    }
+  }
+
+  render() {
+    return <div class="login">{this.user()}</div>;
+  }
+}
