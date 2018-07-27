f3ac6e1624a8b96326f437e4c2aa3afcae46a4f3
Rutabadev <etienner37@gmail.com>
2018-07-27 14:46:38 +0200

    footer down on responsive

--- src/header/sidenav/index.js
+++ src/header/sidenav/index.js
@@ -24,6 +24,7 @@ export const SideNav = ({ features, sideNavDisplay, closeHandler }) => {
         </div>
       </div>
       {links}
+      <button class="theme-switcher">Switch theme</button>
     </div>
   );
 };
