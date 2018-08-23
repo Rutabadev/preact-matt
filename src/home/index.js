a71ac6a569838d8df1465dc456488551dd37acff
Rutabadev <etienner37@gmail.com>
2018-08-23 17:15:07 +0200

    debut de bio

--- src/home/index.js
+++ src/home/index.js
@@ -1,7 +1,18 @@
+import "./style.scss"
+
 export const Home = () => {
   return (
     <div class="home">
-      <p>C'est la maison</p>
+      <h1 align="center">Bienvenue sur le site de Matthieu <i>"Puex"</i> Montaillé</h1>
+      <div class="main">
+        <br/><br/>
+        <h2>Biographie</h2>
+        <p>De son vrai prénom Kim Jun Ho, Matthieu Montaillé est un jeune <strike>
+          immigré
+        </strike> coréen natif, devenu catalan de force par une adoption dont les détails resteront secret tellements ils vous regardent pas alors calmez vous bien.</p>
+        <br/>
+        <p>Après une enfance difficile mais somme toute aisée, il devient adulte.</p>
+      </div>
     </div>
   );
 };
