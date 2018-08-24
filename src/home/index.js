d12589dfbe321957135ee855f95471a808f3377e
Rutabadev <etienner37@gmail.com>
2018-08-24 14:25:17 +0200

    moar txt

--- src/home/index.js
+++ src/home/index.js
@@ -17,6 +17,9 @@ export const Home = () => {
         <p>S'en suit alors une suite de péripéties tumulteuses sans intérêt ni saveur. Par exemple une fois il s'est cogné l'orteil contre le coin de son lit. C'est douleureux.</p>
         <br/>
         <p>C'est sur les coups de l'année 1789 que Matthieu fit une découverte sans prédédent : il n'était pas encore né. Ça fait un choc sur le moment mais c'est pas déconnant quand on y réfléchit bien.</p>
+        <br/>
+        <h2>Œuvre</h2>
+        <p>On ne va pas se mentir, il a pas fait grand chose. On retiendra principalement ses classements au badmington. Une fois il a fait 16ème je crois, c'est pas dégueu.</p>
       </div>
     </div>
   );
