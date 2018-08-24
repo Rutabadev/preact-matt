ff4afb26129bbdc7d2ff8e49f6b6b85981626c0e
Rutabadev <etienner37@gmail.com>
2018-08-24 10:42:21 +0200

    more text and justify

--- src/home/index.js
+++ src/home/index.js
@@ -5,13 +5,18 @@ export const Home = () => {
     <div class="home">
       <h1 align="center">Bienvenue sur le site de Matthieu <i>"Puex"</i> Montaillé</h1>
       <div class="main">
-        <br/><br/>
+        <br/>
+        <br/>
         <h2>Biographie</h2>
         <p>De son vrai prénom Kim Jun Ho, Matthieu Montaillé est un jeune <strike>
           immigré
         </strike> coréen natif, devenu catalan de force par une adoption dont les détails resteront secret tellements ils vous regardent pas alors calmez vous bien.</p>
         <br/>
         <p>Après une enfance difficile mais somme toute aisée, il devient adulte.</p>
+        <br/>
+        <p>S'en suit alors une suite de péripéties tumulteuses sans intérêt ni saveur. Par exemple une fois il s'est cogné l'orteil contre le coin de son lit. C'est douleureux.</p>
+        <br/>
+        <p>C'est sur les coups de l'année 1789 que Matthieu fit une découverte sans prédédent : il n'était pas encore né. Ça fait un choc sur le moment mais c'est pas déconnant quand on y réfléchit bien.</p>
       </div>
     </div>
   );
