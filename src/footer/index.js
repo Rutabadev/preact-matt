efcf78a09c76040359bb5299d332ef0c446cb98b
Etienne <etienner37@gmail.com>
2018-09-10 09:58:33 +0200

    footer links fix

--- src/footer/index.js
+++ src/footer/index.js
@@ -15,16 +15,18 @@ export class Footer extends Component {
   render () {
     return (
       <div class='footer'>
-        <Roller content='Githeub' link='https://github.com/Puex' />
-        <Roller content='Youtoube' link='https://www.youtube.com/user/OnchayTM' />
-        <Roller content='Touitteur' link='https://twitter.com/MZPuex' />
-        <Roller content='Sith' link='/' onClick={(e) => {
-          e.preventDefault()
-          this.setState({ modalOpen: true })
-        }
-        } />
-        <Roller content='Einstagram' link='https://www.instagram.com/matt_mntl' />
-        <Roller content='Blaugue' link='https://cestmoijuliettevoyons6.webnode.fr/' />
+        <div class='links'>
+          <Roller content='Githeub' link='https://github.com/Puex' />
+          <Roller content='Youtoube' link='https://www.youtube.com/user/OnchayTM' />
+          <Roller content='Touitteur' link='https://twitter.com/MZPuex' />
+          <Roller content='Sith' link='/' onClick={(e) => {
+            e.preventDefault()
+            this.setState({ modalOpen: true })
+          }
+          } />
+          <Roller content='Einstagram' link='https://www.instagram.com/matt_mntl' />
+          <Roller content='Blaugue' link='https://cestmoijuliettevoyons6.webnode.fr/' />
+        </div>
         <Modal title='Sith' onSuccess={() => this.setState({ modalOpen: false })} type={MODAL_TYPES.WARNING} modalOpen={this.state.modalOpen}>
           <p>Alors en fait vous allez rire mais vous êtes actuellement sur le site</p>
         </Modal>
