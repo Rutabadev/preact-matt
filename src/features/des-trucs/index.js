8b246188eb6b30f60c31ca73325eb7eaeab886c1
Rutabadev <etienner37@gmail.com>
2018-09-27 10:27:09 +0200

    waka waka héhé

--- src/features/des-trucs/index.js
+++ src/features/des-trucs/index.js
@@ -14,31 +14,5 @@ export const DesTrucs = () => (
                 <p>Ou encore ça</p><span class="nazi">&#21325;</span>
             </div>
         </div>
-        {/* <table align="center">
-            <tr>
-                <td>
-                    <p>Genre ça</p>
-                </td>
-                <td>
-                    <span>☯</span>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>Ça</p>
-                </td>
-                <td>
-                    <span>⚡</span>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>Ou encore ça</p>
-                </td>
-                <td>
-                    <span class="nazi">&#21325;</span>
-                </td>
-            </tr>
-        </table> */}
     </div>
 )
--- src/features/home/index.js
+++ src/features/home/index.js
@@ -17,6 +17,7 @@ export const Home = () => (
       <h2>Œuvre</h2>
       <p>On ne va pas se mentir, il a pas fait grand chose. On retiendra principalement ses classements au badmington. Une fois il a fait 16ème je crois, c'est pas dégueu.</p>
       <br />
+      <img class="waka" src="../../assets/wakahr.png"></img>
       <h2>Conclusion</h2>
       <p>--. En fait c'est un smiley qui le représente bien parcequ'il vient du 66 et qu'il faut appuyer deux fois sur le 6 du clavier pour faire ça. Aussi ça lui ressemble parcequ'il est bridé.</p>
     </div>
