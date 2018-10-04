32bca7b0908d5bf1623419eb668979122591961f
Rutabadev <etienner37@gmail.com>
2018-10-04 10:29:31 +0200

    you are here feature

--- src/features/clicks/index.js
+++ src/features/clicks/index.js
@@ -6,14 +6,15 @@ export class Clicks extends Component {
     constructor() {
         super()
         this.state = {
-            lastClick: null
+            lastClick: null,
+            lastPosition: null
         }
 
         this.showClick = this.showClick.bind(this);
+        this.showPosition = this.showPosition.bind(this);
     }
 
     showClick(event) {
-        console.log(event.clientX);
         let click = (
             <div class="click" style={{ left: event.clientX, top: event.clientY }}>
                 <div class="arrow_box">You clicked here</div>
@@ -24,10 +25,24 @@ export class Clicks extends Component {
         })
     }
 
+    showPosition(event) {
+        console.log(`1 = x: ${event.pageX}, y: ${event.pageY}`)
+        console.log(`2 = x: ${event.nativeEvent.clientX}, y: ${event.nativeEvent.clientY}`)
+        let position = (
+            <div class="click position" style={{ left: event.pageX, top: event.nativeEvent.pageY }}>
+                <div class="arrow_box">You are here</div>
+            </div>
+        )
+        this.setState({
+            lastPosition: position
+        })
+    }
+
     render() {
         return (
-            <div class="clicks" onClick={(e) => this.showClick(e)}>
+            <div class="clicks" onClick={(e) => this.showClick(e)} onMouseMove={(e) => this.showPosition(e)}>
                 {this.state.lastClick}
+                {this.state.lastPosition}
             </div>
         )
     }
