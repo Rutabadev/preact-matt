29cdaebd512b5bbfd0d08372ec26aa52bf045268
Rutabadev <etienner37@gmail.com>
2018-10-24 13:50:35 +0200

    parallax

--- src/features/home/parallax/index.js
+++ src/features/home/parallax/index.js
@@ -1,7 +1,61 @@
 import './style.scss'
+import { Parallax, Background } from 'react-parallax'
 
-export const Parallax = () => (
+const insideStyles = {
+    background: "white",
+    padding: 20,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)"
+};
+
+
+export const ParallaxMatt = () => (
     <div class="parallax">
-        <p>Allo le parallax</p>
+        <Parallax bgImage={'https://source.unsplash.com/1600x900/?nature,water'} strength={500}>
+            <div style={{ height: 500 }}>
+                <div style={insideStyles}>Matthieu est beau</div>
+            </div>
+        </Parallax>
+        <h1>| | |</h1>
+        <Parallax bgImage={'https://source.unsplash.com/1600x900/?animal'} blur={{ min: -1, max: 3 }}>
+            <div style={{ height: 500 }}>
+                <div style={insideStyles}>Matthieu est frais</div>
+            </div>
+        </Parallax>
+        <h1>| | |</h1>
+        <Parallax bgImage={'https://source.unsplash.com/1600x900/?mountain'} strength={-100}>
+            <div style={{ height: 500 }}>
+                <div style={insideStyles}>Mais surtout</div>
+            </div>
+        </Parallax>
+        <h1>| | |</h1>
+        <Parallax
+            bgImage={'https://source.unsplash.com/1600x900/?dog'}
+            strength={200}
+            renderLayer={percentage => (
+                <div>
+                    <div
+                        style={{
+                            position: "absolute",
+                            background: `rgba(255, 220, 6, ${percentage * 1})`,
+                            left: "50%",
+                            top: "50%",
+                            borderRadius: "50%",
+                            transform: "translate(-50%,-50%)",
+                            width: percentage * 500,
+                            height: percentage * 500
+                        }}
+                    />
+                </div>
+            )}
+        >
+            <div style={{ height: 500 }}>
+                <div style={insideStyles}>Matthieu est bridé \u2728</div>
+            </div>
+        </Parallax>
+        <div style={{ height: 370 }} />
+        <h2>{"\u2728"}</h2>
     </div>
 )
