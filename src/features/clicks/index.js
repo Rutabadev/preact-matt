ff7e2b95c71532b83c28a556fbc3cd125222a126
Etienne <etienner37@gmail.com>
2018-11-13 12:58:06 +0100

    new game +

--- src/features/clicks/index.js
+++ src/features/clicks/index.js
@@ -1,49 +1,98 @@
-import './style.scss'
-import { Component } from 'preact'
+import "./style.scss";
+import { Component } from "preact";
 
 export class Clicks extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lastClick: null,
+      lastPosition: null,
+      allowPosition: null,
+      score: 0
+    };
 
-    constructor() {
-        super()
-        this.state = {
-            lastClick: null,
-            lastPosition: null,
-            allowPosition: null
-        }
+    this.showClick = this.showClick.bind(this);
+    this.showPosition = this.showPosition.bind(this);
+  }
 
-        this.showClick = this.showClick.bind(this);
-        this.showPosition = this.showPosition.bind(this);
-    }
-
-    showClick(event) {
-        let click = (
-            <div class="click" style={{ left: event.clientX, top: event.clientY }}>
-                <div class="arrow_box">You clicked here</div>
-            </div>
-        )
+  componentDidMount() {
+    const clicksDiv = document.getElementById("clicks");
+    this.setState({ clicksDivHeight: clicksDiv.clientHeight });
+    this.setState({ clicksDivWidth: clicksDiv.clientWidth });
+    this.interval = setInterval(
+      () =>
         this.setState({
-            lastClick: click
-        })
-    }
+          btnX: Math.random() * (this.state.clicksDivWidth - 30),
+          btnY: Math.random() * (this.state.clicksDivHeight - 30),
+          buttonClickable: true
+        }),
+      1800
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
-    showPosition(event) {
-        if (this.props.device !== 'desktop') return
-        let position = (
-            <div class="click position" style={{ left: event.pageX, top: event.nativeEvent.pageY }}>
-                <div class="arrow_box">You are here</div>
-            </div>
-        )
-        this.setState({
-            lastPosition: position
-        })
-    }
+  showClick(event) {
+    let click = (
+      <div class="click" style={{ left: event.clientX, top: event.clientY }}>
+        <div class="arrow_box">You clicked here</div>
+      </div>
+    );
+    this.setState({
+      lastClick: click
+    });
+  }
 
-    render({ device }) {
-        return (
-            <div class="clicks" onClick={(e) => this.showClick(e)} onMouseEnter={() => this.setState({ allowPosition: true })} onMouseLeave={() => this.setState({ allowPosition: false })} onMouseMove={(e) => this.showPosition(e)}>
-                {this.state.lastClick}
-                {(device === 'desktop' && this.state.allowPosition) ? this.state.lastPosition : null}
-            </div>
-        )
+  showPosition(event) {
+    if (this.props.device !== "desktop") return;
+    let position = (
+      <div
+        class="click position"
+        style={{ left: event.pageX, top: event.nativeEvent.pageY }}
+      >
+        <div class="arrow_box">You are here</div>
+      </div>
+    );
+    this.setState({
+      lastPosition: position
+    });
+  }
+
+  handleAim() {
+    if (this.state.buttonClickable) {
+      this.setState({ score: this.state.score + 1 });
+      this.setState({ buttonClickable: false });
     }
+  }
+
+  render({ device }) {
+    return (
+      <div
+        id="clicks"
+        class="clicks"
+        onClick={e => this.showClick(e)}
+        onMouseEnter={() => this.setState({ allowPosition: true })}
+        onMouseLeave={() => this.setState({ allowPosition: false })}
+        onMouseMove={e => this.showPosition(e)}
+      >
+        <p>Score : {this.state.score}</p>
+        {this.state.lastClick}
+        {device === "desktop" && this.state.allowPosition
+          ? this.state.lastPosition
+          : null}
+        <button
+          onClick={() => this.handleAim()}
+          class="secondary aim"
+          style={{
+            width: "30px",
+            height: "30px",
+            padding: "0",
+            left: `${this.state.btnX}px`,
+            top: `${this.state.btnY}px`
+          }}
+        />
+      </div>
+    );
+  }
 }
