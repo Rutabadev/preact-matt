231b678922cacced2f5f268fe72a0fd993944e9e
Rutabadev <etienner37@gmail.com>
2019-01-07 10:50:37 +0100

    politiquement ok

--- src/features/des-trucs/index.js
+++ src/features/des-trucs/index.js
@@ -1,18 +1,21 @@
-import './style.scss';
+import "./style.scss";
 
 export const DesTrucs = () => (
-    <div class="trucs">
-        <h1>Des trucs</h1>
-        <div class="flex-wrapper">
-            <div class="row">
-                <p>Genre ça</p><span>☯</span>
-            </div>
-            <div class="row">
-                <p>Ça</p><span>⚡</span>
-            </div>
-            <div class="row">
-                <p>Ou encore ça</p><span class="nazi">&#21325;</span>
-            </div>
-        </div>
+  <div class="trucs">
+    <h1>Des trucs</h1>
+    <div class="flex-wrapper">
+      <div class="row">
+        <p>Genre ça</p>
+        <span>♥</span>
+      </div>
+      <div class="row">
+        <p>Ça</p>
+        <span>⚡</span>
+      </div>
+      <div class="row">
+        <p>Ou encore ça</p>
+        <span class="yinyang">☯</span>
+      </div>
     </div>
-)
+  </div>
+);
