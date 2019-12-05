9da30478a5664fac24e448604a3e87de3716806d
Etienne Raby <etienne.raby@fittingbox.com>
2019-12-05 12:20:37 +0100

    refacto shadowfun

--- src/features/shadow/index.js
+++ src/features/shadow/index.js
@@ -12,38 +12,27 @@ export class Shadow extends Component {
     }
 
     render() {
-        return(
+
+        function row(ceci, property) {
+            return (
+                <div class="row">
+                    <button class="tertiary" onClick={() => ceci.setState({ [property]: ceci.state[property] - 100 })}>---</button>
+                    <button class="tertiary" onClick={() => ceci.setState({ [property]: ceci.state[property] - 10 })}>--</button>
+                    <button class="tertiary" onClick={() => ceci.setState({ [property]: ceci.state[property] - 1 })}>-</button>
+                    <span>{property}</span>
+                    <button class="tertiary" onClick={() => ceci.setState({ [property]: ceci.state[property] + 1 })}>+</button>
+                    <button class="tertiary" onClick={() => ceci.setState({ [property]: ceci.state[property] + 10 })}>++</button>
+                    <button class="tertiary" onClick={() => ceci.setState({ [property]: ceci.state[property] + 100 })}>+++</button>
+                </div>
+            )
+        }
+
+        return (
             <div class="shadow">
-                <div class="card" style={{boxShadow: `${this.state.horizontal}px ${this.state.vertical}px ${this.state.blur}px var(--shadow-color)`}}>
-                    <div class="row">
-                        <button class="tertiary" onClick={() => this.setState({blur: this.state.blur - 100})}>---</button>
-                        <button class="tertiary" onClick={() => this.setState({blur: this.state.blur - 10})}>--</button>
-                        <button class="tertiary" onClick={() => this.setState({blur: this.state.blur - 1})}>-</button>
-                        <span>blur</span>
-                        <button class="tertiary" onClick={() => this.setState({blur: this.state.blur + 1})}>+</button>
-                        <button class="tertiary" onClick={() => this.setState({blur: this.state.blur + 10})}>++</button>
-                        <button class="tertiary" onClick={() => this.setState({blur: this.state.blur + 100})}>+++</button>
-                    </div>
-
-                    <div class="row">
-                        <button class="tertiary" onClick={() => this.setState({horizontal: this.state.horizontal - 100})}>---</button>
-                        <button class="tertiary" onClick={() => this.setState({horizontal: this.state.horizontal - 10})}>--</button>
-                        <button class="tertiary" onClick={() => this.setState({horizontal: this.state.horizontal - 1})}>-</button>
-                        <span>horizontal</span>
-                        <button class="tertiary" onClick={() => this.setState({horizontal: this.state.horizontal + 1})}>+</button>
-                        <button class="tertiary" onClick={() => this.setState({horizontal: this.state.horizontal + 10})}>++</button>
-                        <button class="tertiary" onClick={() => this.setState({horizontal: this.state.horizontal + 100})}>+++</button>
-                    </div>
-
-                    <div class="row">
-                        <button class="tertiary" onClick={() => this.setState({vertical: this.state.vertical + 100})}>---</button>
-                        <button class="tertiary" onClick={() => this.setState({vertical: this.state.vertical + 10})}>--</button>
-                        <button class="tertiary" onClick={() => this.setState({vertical: this.state.vertical + 1})}>-</button>
-                        <span>vertical</span>
-                        <button class="tertiary" onClick={() => this.setState({vertical: this.state.vertical - 1})}>+</button>
-                        <button class="tertiary" onClick={() => this.setState({vertical: this.state.vertical - 10})}>++</button>
-                        <button class="tertiary" onClick={() => this.setState({vertical: this.state.vertical - 100})}>+++</button>
-                    </div>
+                <div class="card" style={{ boxShadow: `${this.state.horizontal}px ${this.state.vertical}px ${this.state.blur}px var(--shadow-color)` }}>
+                    {row(this, 'blur')}
+                    {row(this, 'horizontal')}
+                    {row(this, 'vertical')} 
                 </div>
             </div>
         )
